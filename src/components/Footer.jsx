5ac590c43d5534154d5873f00060adaef299ddae
Sezam <dossumbekov@list.ru>
2022-08-30 02:07:21 +0600

    aded: github repo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
     <div className="footer-copyright">
       <div className="container">
       © {new Date().getFullYear()} Copyright Text
-      <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+      <a className="grey-text text-lighten-4 right" href="https://github.com/sezamiti/ShowCase" target='_blank' rel="noreferrer">Repo</a>
       </div>
     </div>
   </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ function Header () {
     <div className="nav-wrapper">
       <a href="index.html" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="!#">Repo</a></li>
+        <li><a href="https://github.com/sezamiti/ShowCase" target='_blank' rel="noreferrer">Repo</a></li> 
         
       </ul>
     </div>
